0eeb46d908b49ede4f7fec93884c8adb845e00b5
elliotxx <951376975@qq.com>
2019-10-18 17:24:13 +0800

    [api] add Field `Email` in Model Job

--- api/api/models.go
+++ api/api/models.go
@@ -15,6 +15,7 @@ type Job struct {
 	Url      string `json:"url" gorm:"type:varchar(1000)"`      // 目标页面 URL
 	Pattern  string `json:"pattern" gorm:"type:varchar(1000)"`  // 抓取规则
 	Charset  string `json:"charset" gorm:"type:varchar(100)"`   // 目标页面编码
+	Email    string `json:"email" gorm:"not null"`              // 通知账户
 	Content  string `json:"content" gorm:"type:varchar(2000)"`  // 邮件内容
 	Status   int    `json:"status"`                             // 运行状态, 0代表“运行中”、1代表“暂停”
 	EntryID  int    `json:"entryId" gorm:"not null"`            // cron 调度器的 job id
