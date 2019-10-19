de256387c05b409b6d05384ed26d6eeea8b5eb0a
elliotxx <951376975@qq.com>
2019-10-19 13:24:21 +0800

    [ui] update rootPath to production environment

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,9 +1,9 @@
 export const globalConfig = {
     // 后端 api 根地址
     // 测试环境
-    rootPath : 'http://localhost:8080',
+    // rootPath : 'http://localhost:8080',
     // 线上环境
-    // rootPath : '',
+    rootPath : '',
 
     // 主页标题
     rootTitle : 'Watchman 更夫'
